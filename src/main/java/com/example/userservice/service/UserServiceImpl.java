bc502794a2b63701b047ac3a5dc454e03b20bfa7
ita-jp <tn00gm@gmail.com>
2022-08-06 23:12:51 +0900

    UserService & UserServiceImpl

---
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.userservice.service;
+
+import com.example.userservice.domain.Role;
+import com.example.userservice.domain.User;
+import com.example.userservice.repo.RoleRepo;
+import com.example.userservice.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        var user = userRepo.findByUsername(username);
+        var role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+}
