a6e7b4fc42fcad49e64bf46d4b5bf9afbac18f94
ita-jp <tn00gm@gmail.com>
2022-08-08 21:12:22 +0900

    authorization error

--- src/main/java/com/example/userservice/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/example/userservice/filter/CustomAuthorizationFilter.java
@@ -2,6 +2,10 @@ package com.example.userservice.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,9 +17,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+@Slf4j
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
@@ -42,8 +48,16 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                     filterChain.doFilter(request, response);
                 } catch (Exception e) {
-
+                    log.error("Error logging in: {}", e.getMessage());
+                    response.setHeader("error", e.getMessage());
+                    response.setStatus(HttpStatus.FORBIDDEN.value());
+                    var error = new HashMap<String, String>();
+                    error.put("error_message", e.getMessage());
+                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                    new ObjectMapper().writeValue(response.getOutputStream(), error);
                 }
+            } else {
+                filterChain.doFilter(request, response);
             }
         }
     }
